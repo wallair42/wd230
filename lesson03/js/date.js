d011dd1710784bb7fab0f7ff543e699091c4b954
wallair42 <pow18001@byui.edu>
2022-01-19 21:45:47 -0700

    copied date js into lesson03

---
+++ lesson03/js/date.js
@@ -0,0 +1,35 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = dayName + ", " + monthName + " " + d.getDate() +
+    ", " + d.getFullYear();
+
+document.getElementById("current-year").innerHTML = d.getFullYear();
+
+let lastModified = document.lastModified;
+document.getElementById("last-modified").innerHTML = lastModified;
