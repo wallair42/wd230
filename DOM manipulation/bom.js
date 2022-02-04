605d288f7fbf6b46682575631b0074d5dfbcf254
wallair42 <pow18001@byui.edu>
2022-02-03 21:06:21 -0700

    completed assignemnt requirments

--- DOM manipulation/bom.js
+++ DOM manipulation/bom.js
@@ -1,19 +1,23 @@
 const list = document.querySelector('ul');
-      const input = document.querySelector('input');
-      const button = document.querySelector('button');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
-      button.onclick = function() {
-        let myItem = input.value;
-        input.value = '';
+button.addEventListener('click', function() {}, false)
 
-        const listItem = document.createElement('li');
+
+button.onclick = function() {
+    let myItem = input.value;
+    input.value = '';
+     
+
+    const listItem = document.createElement('li');
         const listText = document.createElement('span');
         const listBtn = document.createElement('button');
 
         listItem.appendChild(listText);
         listText.textContent = myItem;
         listItem.appendChild(listBtn);
-        listBtn.textContent = 'Delete';
+        listBtn.textContent = '\u274C';
         list.appendChild(listItem);
 
         listBtn.onclick = function(e) {
